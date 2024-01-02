e82cadcc47d4be9dea23901bc1d253a90783cf14
Bhagat Gurung <bhagattamu@gmail.com>
2024-01-03 00:14:40 +1100

    Add logger
    Added winston logger

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { mongo } = require("./vars");
 
 mongoose.connection.on("error", (err) => {
-  console.error(`MongoDB connection error: ${err}`);
+  logger.error(`MongoDB connection error: ${err}`);
   process.exit(1);
 });
 
