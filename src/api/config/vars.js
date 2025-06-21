f78f39bdf05d037a1a90cc81dfd2065dbbc91766
Bhagat Gurung <bhagattamu@gmail.com>
2025-06-22 00:45:53 +1000

    Set up docker for the project

--- src/api/config/vars.js
+++ src/api/config/vars.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+
 const vars = {
   port: process.env.PORT || 3000,
-  env: process.env.NODE_ENV || "dev",
+  env: process.env.NODE_ENV || "development",
   apiVersion: process.env.VERSION || 1,
   mongo: {
-    uri: process.env.MONGO_URI || process.env.MONGO_TEST_URI,
+    uri: `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@mongo:${DB_PORT}/${DB_NAME}?authSource=admin`,
   },
   jwt: {
     secret: "testsecret",
