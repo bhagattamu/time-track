205854148115380cb000395d4055c5ed904c9b02
Bhagat Gurung <bhagattamu@gmail.com>
2025-06-22 00:43:12 +1000

    commented expressMongoSanitize middleware - issue with express 5 due to mutation in query

--- src/api/config/express.js
+++ src/api/config/express.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const ExpressMongoSanitize = require("express-mongo-sanitize");
+// const ExpressMongoSanitize = require("express-mongo-sanitize");
 const { default: helmet } = require("helmet");
 const compression = require("compression");
-const httpStatus = require("http-status");
+const { status: httpStatus } = require("http-status");
 const passport = require("passport");
 const { apiVersion, env } = require("./vars");
 const routes = require(`../v${apiVersion}/routes`);
@@ -13,6 +13,9 @@ const ApiError = require("../utils/ApiError");
 const morgan = require("./morgan");
 
 const app = express();
+
+// sanitize request data
+// app.use(ExpressMongoSanitize());
 
 if (env !== "test") {
   app.use(morgan.successHandler);
@@ -25,9 +28,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// sanitize request data
-app.use(ExpressMongoSanitize());
 
 // gzip compression
 app.use(compression());
